34adf2a290ef82fc17d50dfa74537c4f4ce5fe21
Augusto Miguel Zeponi <augustozeponi@hotmail.com>
2022-07-12 23:47:24 -0300

    -- Commit Inicial --

---
+++ Models/ClienteContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace APIDrugovich.Models
+{
+    public class ClienteContext : DbContext
+    {
+        public ClienteContext(DbContextOptions<ClienteContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Clientes> Clientes {get; set;} = null!;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using APIDrugovich.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ClienteContext>(opt =>
+    opt.UseInMemoryDatabase("Clientes"));
+
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
